dc53a2539ff9b0f3c67f7a6a70c8b7b4b8db050e
Steve Fenton <commits@stevefenton.co.uk>
2014-11-07 23:43:57 +0000

    NuSpec file for external package and FizzBuzzTests.ts example file.

---
+++ tsUnit.External.Examples/Scripts/FizzBuzzTests.ts
@@ -0,0 +1,30 @@
+import tsUnit = require('./tsUnit/tsUnit');
+import FizzBuzz = require('./FizzBuzz');
+
+var target = new FizzBuzz();
+
+export class FizzBuzzTests extends tsUnit.TestClass {
+
+    normalNumbersReturnOriginalNumber() {
+        this.areIdentical(1, target.generate(1));
+        this.areIdentical(2, target.generate(2));
+        this.areIdentical(4, target.generate(4));
+    }
+
+    numberDivisibleByThreeShouldReturnFizz() {
+        this.areIdentical("Fizz", target.generate(3));
+        this.areIdentical("Fizz", target.generate(6));
+        this.areIdentical("Fizz", target.generate(9));
+    }
+
+    numbersDivisibleByFiveShouldReturnBuzz() {
+        this.areIdentical("Buzz", target.generate(5));
+        this.areIdentical("Buzz", target.generate(10));
+        this.areIdentical("Buzz", target.generate(20));
+    }
+    numbersDivisibleByThreeAndFiveShouldReturnFizzBuzz() {
+        this.areIdentical("FizzBuzz", target.generate(15));
+        this.areIdentical("FizzBuzz", target.generate(30));
+        this.areIdentical("FizzBuzz", target.generate(45));
+    }
+}
